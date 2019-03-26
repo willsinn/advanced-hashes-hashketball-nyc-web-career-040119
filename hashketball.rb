3eeba104f159375fa7d1b05fb92f25dc32bb1806
w.h.sinn <w.h.sinn@gmail.com>
2019-03-26 18:46:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,8 +118,9 @@ def game_hash
 end
 
 def num_points_scored(name)
-  players = game_hash[:home][:players].merge(game_hash[:away][:players].fetch(name)
-  print player.fetch(:points)
+  players = game_hash[:home][:players].merge(game_hash[:away][:players]
+  player = players.fetch(name)
+  player.fetch(:points)
   
 end
 
